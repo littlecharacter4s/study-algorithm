2b2e1e84649647834479e0e87f58796280749a79
gujixian <gujixian@daojia.com>
2018-09-23 22:50:58 +0800

    广度优先遍历二叉树

--- src/main/java/com/lc/algorithm/N0102BinaryTreeLevelOrderTraversal.java
+++ src/main/java/com/lc/algorithm/N0102BinaryTreeLevelOrderTraversal.java
@@ -1,52 +1,32 @@
 package com.lc.algorithm;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.Queue;
 
 public class N0102BinaryTreeLevelOrderTraversal {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        ConcurrentLinkedQueue<TreeNode> queue = new ConcurrentLinkedQueue<>();
-        if (root == null) {
+        if(root == null){
             return result;
         }
-        result.add(Collections.singletonList(root.val));
-        if (root.left == null && root.right == null) {
-            return result;
-        }
-        if (root.left != null) {
-            queue.offer(root.left);
-        }
-        if (root.right != null) {
-            queue.offer(root.right);
-        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
         queue.offer(root);
-        List<Integer> list = new ArrayList<>();
-        boolean flag = false;
-        while(queue.peek() != null) {
-            TreeNode node = queue.poll();
-            if (node == root) {
-                if (!list.isEmpty()) {
-                    result.add(list);
-                    list = new ArrayList<>();
+        while(!queue.isEmpty()){
+            int levelNum = queue.size();
+            List<Integer> subList = new ArrayList<>();
+            for (int i = 0; i < levelNum; i++) {
+                TreeNode node = queue.poll();
+                subList.add(node.val);
+                if(node.left != null){
+                    queue.offer(node.left);
                 }
-                if (flag) {
-                    queue.offer(root);
-                    flag = false;
+                if(node.right != null){
+                    queue.offer(node.right);
                 }
-                continue;
             }
-            list.add(node.val);
-            if (node.left != null) {
-                queue.offer(node.left);
-                flag = true;
-            }
-            if (node.right != null) {
-                queue.offer(node.right);
-                flag = true;
-            }
+            result.add(subList);
         }
         return result;
     }
