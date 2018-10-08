47d98b437cc41b5de0dc58b9b23df9979805689f
gujixian <gujixian@daojia.com>
2018-10-08 21:24:54 +0800

    计算四则运算表达式

--- src/main/java/com/lc/algorithm/NaaaeCalculateArithmeticExpression.java
+++ src/main/java/com/lc/algorithm/NaaaeCalculateArithmeticExpression.java
@@ -2,6 +2,17 @@ package com.lc.algorithm;
 
 import com.lc.algorithm.stack.Stack;
 
+/**
+ * 计算四则运算表达式
+ * 第一步：将中缀表达转换成后缀表达式；
+ * 1.当当前字符为数字时，直接输出；
+ * 2.当当前字符为"("时，将其压栈；
+ * 3.当当前字符为")"时，则弹出堆栈中最上的"("之前的所有运算符并输出，然后删除堆栈中的"("；
+ * 4.当当前字符为运算符时，则依次弹出堆栈中优先级大于等于当前运算符的(到"("之前为止)运算符，输出，再将当前运算符压栈；
+ * 5.最后弹出所有栈中的内容输出。
+ * 第二步：计算后缀表达式。
+ * 从左到右遍历表达式的每个数字和符号，遇到数字就进栈，遇到是符号，就将处于栈顶的两个数字出栈，进行计算，然后计算结果进栈，一直到最终获得结果。
+ */
 public class NaaaeCalculateArithmeticExpression {
     public float calculate(String expression) {
         float result = 0.0F;
