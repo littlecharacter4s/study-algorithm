91453ce207aea4a423bbcf289de07ddb1e677b4a
gujixian <gujixian@daojia.com>
2018-10-28 21:18:31 +0800

    LUR算法

---
+++ src/test/java/com/lc/algorithm/NaaagLurTest.java
@@ -0,0 +1,24 @@
+package com.lc.algorithm;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+public class NaaagLurTest {
+    @Test
+    public void testLur() throws Exception {
+        //指定缓存最大容量为4
+        Map<Integer,Integer> map=new NaaagLur<>(4);
+        map.put(9,3);
+        map.put(7,4);
+        map.put(5,9);
+        map.put(3,4);
+        map.get(9);
+        map.put(6,6);
+        //总共put了5个元素，超过了指定的缓存最大容量
+        //遍历结果
+        for (Map.Entry<Integer, Integer> integerIntegerEntry : map.entrySet()) {
+            System.out.println(integerIntegerEntry.getKey());
+        }
+    }
+}
