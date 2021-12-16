11e0fe3c90eb94fee341f90a6e2d6d418b1573cc
gujixian <gujixian@meituan.com>
2021-12-16 16:50:55 +0800

    Modify MinHeapTest

--- src/test/java/com/lc/algorithm/tree/heap/MinHeapTest.java
+++ src/test/java/com/lc/algorithm/tree/heap/MinHeapTest.java
@@ -14,12 +14,15 @@ public class MinHeapTest {
         minHeap.offer(24);
         minHeap.offer(35);
         minHeap.offer(36);
-        minHeap.offer(30);
         minHeap.offer(25);
+        minHeap.offer(30);
         int[] elements = minHeap.getElements();
         System.out.println(JSON.toJSONString(elements));
-        while (minHeap.size() != 0) {
-            System.out.println(minHeap.poll());
-        }
+        // while (minHeap.size() != 0) {
+        //     System.out.println(minHeap.poll());
+        // }
+        minHeap.poll();
+        elements = minHeap.getElements();
+        System.out.println(JSON.toJSONString(elements));
     }
 }
