7cea3940199c1a1075341a01703ffc82e3798b56
gujixian <littlecharacter@qq.com>
2022-07-14 17:58:03 +0800

    二分查找

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,10 +2,17 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class BaseTest {
     @Test
     public void test() {
+        ArrayBlockingQueue queue = new ArrayBlockingQueue(12);
+        Stack stack = new Stack();
+        LinkedList linkedList = new LinkedList();
         Assert.assertTrue("这只是一个测试", LocalDate.now().getYear() < Integer.MAX_VALUE);
     }
 }
