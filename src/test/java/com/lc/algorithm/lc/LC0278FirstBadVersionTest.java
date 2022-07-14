5720e856ddae20f36d301e346929db77b7b07f4c
gujixian <littlecharacter@qq.com>
2022-07-14 19:40:50 +0800

    二分查找

---
+++ src/test/java/com/lc/algorithm/lc/LC0278FirstBadVersionTest.java
@@ -0,0 +1,17 @@
+package com.lc.algorithm.lc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author gujixian
+ * @since 2022/7/14
+ */
+public class LC0278FirstBadVersionTest {
+    @Test
+    public void testFirstBadVersion() {
+        LC0278FirstBadVersion firstBadVersion = new LC0278FirstBadVersion();
+        System.out.println(firstBadVersion.firstBadVersion(9));
+    }
+}
