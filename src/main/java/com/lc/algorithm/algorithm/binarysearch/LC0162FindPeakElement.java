2c4fbb1af992671cb455e0bc04b1491f9154e013
gujixian <littlecharacter@qq.com>
2022-07-15 09:02:24 +0800

    二分查找

--- src/main/java/com/lc/algorithm/algorithm/binarysearch/LC0162FindPeakElement.java
+++ src/main/java/com/lc/algorithm/algorithm/binarysearch/LC0162FindPeakElement.java
@@ -29,16 +29,19 @@ public class LC0162FindPeakElement {
         if (nums[nums.length - 1] > nums[nums.length - 2]) {
             return nums.length - 1;
         }
-        int left = 0;
-        int right = nums.length - 1;
+        int left = 1;
+        int right = nums.length - 2;
         while (left <= right) {
             int middle = left + ((right - left) >>> 1);
+            if (nums[middle] > nums[middle - 1] && nums[middle] > nums[middle + 1]) {
+                return middle;
+            }
             if (nums[middle] > nums[middle + 1]) {
                 right = middle - 1;
             } else {
                 left = middle + 1;
             }
         }
-        return left;
+        return -1;
     }
 }
