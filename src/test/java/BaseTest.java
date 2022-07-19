5a12ae7170d9e233f8c330d0c145bdc10859ea58
gujixian <littlecharacter@qq.com>
2022-07-19 18:49:38 +0800

    链表

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -6,13 +6,19 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Stack;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class BaseTest {
     @Test
     public void test() {
-        ArrayBlockingQueue queue = new ArrayBlockingQueue(12);
-        Stack stack = new Stack();
-        LinkedList linkedList = new LinkedList();
         Assert.assertTrue("这只是一个测试", LocalDate.now().getYear() < Integer.MAX_VALUE);
+    }
+
+    @Test
+    public void testBase() {
+        LinkedList linkedList = new LinkedList();
+        Stack stack = new Stack();
+        ArrayBlockingQueue queue = new ArrayBlockingQueue(12);
+        LinkedBlockingQueue linkedBlockingQueue = new LinkedBlockingQueue();
     }
 }
