de43767f8f9fa6017c3f174bcc2590baf3496600
gujixian <littlecharacter@qq.com>
2022-11-23 00:17:10 +0800

    排序

--- src/main/java/com/lc/algorithm/Test.java
+++ src/main/java/com/lc/algorithm/Test.java
@@ -1,31 +1,57 @@
 package com.lc.algorithm;
 
+import java.util.Random;
+
 public class Test {
     public static void main(String[] args) {
-        int[][] a = {{1,2,3},{4,5,6,},{7,8,9}};
-        int[][] sum = new int[a.length][a[0].length];
-        sum[0][0] = a[0][0];
-        for (int i = 1; i < a.length; i++) {
-            sum[i][0] = sum[i-1][0] + a[i][0];
-        }
-        for (int j = 1; j < a[0].length; j++) {
-            sum[0][j] = sum[0][j-1] + a[0][j];
-        }
+        int[] nums = new int[]{5,1,1,2,0,0};
+        new Test().sortArray(nums);
 
-        for (int i = 1; i < a.length; i++) {
-            int rowSum = a[i][0];
-            for (int j = 1; j < a.length; j++) {
-                rowSum += a[i][j];
-                sum[i][j] = sum[i-1][j] + rowSum;
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public int[] sortArray(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return nums;
+        }
+        this.quiklySort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void quiklySort(int[] nums, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int[] equal = this.partition(nums, left, right);
+        this.quiklySort(nums, left, equal[0]);
+        this.quiklySort(nums, equal[1], right);
+    }
+
+    private int[] partition(int[] nums, int left, int right) {
+        int pivot = nums[left + new Random().nextInt(right - left + 1)];
+        int i = left;
+        int l = i - 1;
+        int r = right + 1;
+        while (i < r) {
+            if (nums[i] < pivot) {
+                this.swap(nums, ++l, i++);
+            } else if (nums[i] > pivot) {
+                this.swap(nums, i, --r);
+            } else {
+                i++;
             }
         }
+        return new int[]{l,r};
+    }
 
-
-        for (int i = 0; i < sum.length; i++) {
-            for (int j = 0; j < sum[0].length; j++) {
-                System.out.print(sum[i][j] + " ");
-            }
-            System.out.println();
+    private void swap (int[] nums, int i, int j) {
+        if (i == j) {
+            return;
         }
+        nums[i] = nums[i] ^ nums[j];
+        nums[j] = nums[i] ^ nums[j];
+        nums[i] = nums[i] ^ nums[j];
     }
 }
