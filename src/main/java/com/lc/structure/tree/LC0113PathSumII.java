0b6a04f71534b26cfe1a5188b32ec38d41db8216
gujixian <littlecharacter@qq.com>
2022-12-05 17:12:09 +0800

    '二叉树'

--- src/main/java/com/lc/structure/tree/LC0113PathSumII.java
+++ src/main/java/com/lc/structure/tree/LC0113PathSumII.java
@@ -1,9 +1,35 @@
-package com.lc.algorithm.lc;
+package com.lc.structure.tree;
+
+import com.alibaba.fastjson.JSON;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LC0113PathSumII {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        TreeNode l1001 = new TreeNode(4);
+        TreeNode l1002 = new TreeNode(8);
+        root.left = l1001;
+        root.right = l1002;
+        TreeNode l2001 = new TreeNode(11);
+        TreeNode l2002 = new TreeNode(13);
+        TreeNode l2003 = new TreeNode(4);
+        l1001.left = l2001;
+        l1002.left = l2002;
+        l1002.right = l2003;
+        TreeNode l3001 = new TreeNode(7);
+        TreeNode l3002 = new TreeNode(2);
+        TreeNode l3003 = new TreeNode(5);
+        TreeNode l3004 = new TreeNode(1);
+        l2001.left = l3001;
+        l2001.right = l3002;
+        l2003.left = l3003;
+        l2003.right = l3004;
+        List<List<Integer>> result = new LC0113PathSumII().pathSum(root, 22);
+        System.out.println(JSON.toJSONString(result));
+    }
+
     private List<Integer> result = new ArrayList<>();
     private List<List<Integer>> results = new ArrayList<>();
 
@@ -33,7 +59,7 @@ public class LC0113PathSumII {
         result.remove(result.size() - 1);
     }
 
-    static class TreeNode {
+    private static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
