9534c2a6549b1d4005ad5b813cb5db5b3940f621
gujixian <littlecharacter@qq.com>
2022-12-25 01:09:14 +0800

    蓄水池抽样算法

--- src/main/java/com/lc/algorithm/NC0001ReservoirSampling.java
+++ src/main/java/com/lc/algorithm/NC0001ReservoirSampling.java
@@ -7,7 +7,7 @@ import java.util.Random;
  * 步骤：
  * 1.假设数据序列的规模为n，需要采样的数量的为k；
  * 2.首先构建一个可容纳k个元素的数组，将序列的前k个元素放入数组中；
- * 3.然后从第i=k+1个元素开始遍历池，随机从[0,i+1)抽取一个下标r，若r<k，则将r位置的抽样替换为i位置的样本；否则什么都不做。
+ * 3.然后从第 i ∈ [k,n) 个元素开始遍历池，随机从 [0,i] 范围抽取一个下标 x，若 x ∈ [0,k)，则将 x 位置的抽样替换为 i 位置的样本；否则什么都不做。
  *   当遍历完所有元素之后，数组中剩下的元素即为所需采取的样本。
  */
 public class NC0001ReservoirSampling {
@@ -19,9 +19,9 @@ public class NC0001ReservoirSampling {
         }
         // k+1个元素开始进行概率采样
         for (int i = k; i < pool.length; i++) {
-            int r = new Random().nextInt(i + 1);
-            if (r < k) {
-                result[r] = pool[i];
+            int x = new Random().nextInt(i + 1);
+            if (x < k) {
+                result[x] = pool[i];
             }
         }
         return result;
